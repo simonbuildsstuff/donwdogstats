0ce1b3255ee1599b0a4a2bdf13af1f094903054d
Simon Schoof <simon.schoof@posteo.net>
2021-03-17 12:03:33 +0100

    down dog history | loads history from down dog with credentials

--- secrethub.fsx
+++ secrethub.fsx
@@ -1,7 +1,20 @@
 #r "nuget: SecretHub"
+#r "nuget: Http.fs, 5.4.0"
+#r "nuget: FSharp.Data"
+
+open Hopac
+open HttpFs.Client
 
 let shclient = new SecretHub.Client()
 
-let secret = shclient.Resolve("secrethub://simonbuildsstuff/start/hello")
+let secret = shclient.Resolve("secrethub://simonbuildsstuff/downdog/cookiecreds")
 
-printfn "%s" secret
+
+let body =
+  Request.createUrl Post "https://www.downdogapp.com/json/history"
+  |> Request.setHeader (ContentType (ContentType.parse "application/x-www-form-urlencoded").Value)
+  |> Request.setHeader (Custom ("Cookie", secret))
+  |> Request.responseAsString
+  |> run
+
+printfn "Here's the body: %s" body
